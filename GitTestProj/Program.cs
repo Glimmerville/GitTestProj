0c38aa0197f67689dd360e4623c2b1f468504946
Your Name <you@email.com>
2016-03-02 10:08:11 -0500

    Initial commit

---
+++ GitTestProj/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I learned a bunch of stuff on Tuesday.");
+            int whatever = 15;
+            int random = 16;
+            Console.WriteLine(whatever + random);
+            Console.ReadLine();
+        }
+    }
+}
